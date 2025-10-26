f0f76e999bcd4b76cbdf4be88d5ffefe4510c7d5
Abinaya <abivijayakumar419@gmail.com>
2025-10-26 21:19:25 +0530

    Create Simple C programming .c

---
+++ Simple C programming .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Method 1: Direct print
+    printf("50\n");
+
+    // Method 2: Using a variable
+    int num = 50;
+    printf("The number is: %d\n", num);
+
+    // Method 3: Adding two numbers to get 50
+    int a = 20, b = 30;
+    printf("Sum of %d and %d is: %d\n", a, b, a + b);
+
+    return 0;
+}

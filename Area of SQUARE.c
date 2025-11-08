34122255df19f5a0397858d47c98903a92239e4b
Abinaya <abivijayakumar419@gmail.com>
2025-11-08 21:43:40 +0530

    Create Area of SQUARE.c

---
+++ Area of SQUARE.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+clrscr();
+printf("Enter the side of the square :");
+scanf("%d", &a);
+printf("\n Area of the square = %d", a*a);
+getch();
+}

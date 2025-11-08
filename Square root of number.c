6e8ec8cd65b40b24ad8ce808f217e823a244c663
Abinaya <abivijayakumar419@gmail.com>
2025-11-08 21:39:23 +0530

    Create Square root of number.c

---
+++ Square root of number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int n,s;
+printf("Enter the number\n");
+scanf("%d", & n);
+s = sqrt(n);
+printf("The square root of %d= %d\n", n,s);
+getch();
+}
